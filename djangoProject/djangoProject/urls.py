96bbb2c850e3d3a348ec0f3911918bc555cb6f6d
aristokrat991 <geeky.post@bk.ru>
2022-09-06 14:09:08 +0500

    pw4.1 commit

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -21,5 +21,6 @@ from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homePageView)
+    path('', views.index),
+    path('about/', views.index2)
 ]
--- djangoProject/pages/views.py
+++ djangoProject/pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+from django.shortcuts import render
+def index(request):
+    return render(request, "home.html")
+
+def index2(request):
+    return render(request, "about.html")
